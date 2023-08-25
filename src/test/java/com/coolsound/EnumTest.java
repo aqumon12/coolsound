f5992d224295988704e4230f876873d342e9f3de
test <test@test.com>
2023-08-25 18:33:19 +0900

    enum test

--- src/test/java/com/coolsound/EnumTest.java
+++ src/test/java/com/coolsound/EnumTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.coolsound.test.PayType;
 import com.coolsound.test.Status;
 
 public class EnumTest {
@@ -13,6 +14,19 @@ public class EnumTest {
 	}
 	
 	@Test
+	void pay테스트1() {
+		// given
+		String payMethod = "KAKAO";
+		
+		// when
+		// 결제 수단(예:카카오페이)에 대한 결제 종류(예:현금 또는 카드)가 무엇인가?
+		PayType payType = PayType.findByPayMethod(payMethod);
+		
+		// then
+		assertEquals(PayType.CARD, payType);
+	}
+	
+	//@Test
 	void Status테스트() {
 		// given - 준비
 		Status status = getStatus();  // Y
