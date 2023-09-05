b6d2eacd86a403e32826b78876c0643917c6f139
test <aqumon12@naver.com>
2023-09-05 13:30:54 +0900

    인터셉터 #11

--- src/main/java/com/coolsound/config/WebMvcConfig.java
+++ src/main/java/com/coolsound/config/WebMvcConfig.java
@@ -1,15 +1,28 @@
 package com.coolsound.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.coolsound.common.FileManagerService;
+import com.coolsound.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 	
+	@Autowired
+	private PermissionInterceptor interceptor;
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+		.addInterceptor(interceptor)
+		.addPathPatterns("/**")
+		.excludePathPatterns("/static/**", "/error", "/favicon.ico", "/user/sign_out", "/admin/sign_out")
+		;
+	}
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/com/coolsound/interceptor/PermissionInterceptor.java
+++ src/main/java/com/coolsound/interceptor/PermissionInterceptor.java
@@ -1,5 +1,73 @@
 package com.coolsound.interceptor;
 
-public class PermissionInterceptor {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class PermissionInterceptor implements HandlerInterceptor {
+private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws IOException {
+		
+		String uri = request.getRequestURI();
+		logger.info("[$$$$$$$] preHandle uri:{}", uri);
+		
+		HttpSession session = request.getSession();
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		if (userId == null && uri.startsWith("/user/u")) {
+			response.sendRedirect("/user/sign_in_view");
+			return false;
+		}
+		
+		if (userId == null && uri.startsWith("/shop/o")) {
+			response.sendRedirect("/user/sign_in_view");
+			return false;
+		}
+		
+		if (userId != null && uri.startsWith("/user/s")) {
+			 response.sendRedirect("/shop/main_view"); 
+			 return false;
+		}
+		 
+		Integer adminId = (Integer)session.getAttribute("adminId");
+		 
+		if (adminId == null && uri.startsWith("/admin/a")) {
+			response.sendRedirect("/admin/sign_in_view");
+			return false;
+		}
+		
+		if (adminId != null && uri.startsWith("/admin/s")) {
+			 response.sendRedirect("/admin/a/main_view"); 
+			 return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView mav) {
+		
+		logger.info("[##############] postHandle");
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			Exception ex) {
+		
+		logger.info("[@@@@@@@@@@@] afterCompletion");
+	}
 }
